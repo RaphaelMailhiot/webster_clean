3603bdd9bb4b89f5df331b3fb68e6d818ef7e8a8
Raphael <89405047+RaphMailhiot@users.noreply.github.com>
2023-08-07 20:56:26 -0400

    Webster Clean v1.1.1

--- lang/en.php
+++ lang/en.php
@@ -16,22 +16,17 @@ $meta['a-propos'][3] ="en/about/";
 $meta['services'][0] ="Services - $namebase";
 $meta['services'][1] ="$descriptionbaseen";
 $meta['services'][2] ="$keywordsbaseen, services,";
-$meta['services'][3] ="en/our-services/";
+$meta['services'][3] ="en/services/";
 
 $meta['realisations'][0] ="Achievements - $namebase";
 $meta['realisations'][1] ="$descriptionbaseen";
 $meta['realisations'][2] ="$keywordsbaseen, réalisations,";
-$meta['realisations'][3] ="en/our-achievements/";
+$meta['realisations'][3] ="en/achievements/";
 
 $meta['contact'][0] ="Contact - $namebase";
 $meta['contact'][1] ="Contact an employee of our expert team now.";
 $meta['contact'][2] ="contacter, contact,";
-$meta['contact'][3] ="en/contact-us/";
-
-$meta['contact-send'][0] ="Message sent - $namebase";
-$meta['contact-send'][1] ="Contact an employee of our expert team now.";
-$meta['contact-send'][2] ="contact us, contact,";
-$meta['contact-send'][3] ="en/contact-send/";
+$meta['contact'][3] ="en/contact/";
 
 if ($page=="template"){  
 $titre ="SEO ".$category."- $namebase";
